c053c4cf7329a029c2d72496acdfaeac92bd8d32
kicks <kicks@KICKSCAR-LAPTOP>
2018-02-21 16:10:52 +0900

    solved

--- src/prob04/StringUtil.java
+++ src/prob04/StringUtil.java
@@ -2,6 +2,12 @@ package prob04;
 
 public class StringUtil {
 	public static String concatenate( String[] str ) {
+		String result = "";
+		
+		for( String s : str ) {
+			result += s;
+		}
+		
+		return result;
 	}
-	
 }
